99e8a391a354f5555b34480a27d1b7c04e44ea04
Sam Hart <19harts@fremancollege.herts.sch.uk>
2021-03-22 13:43:20 +0000

    Corrected some capitalisation.

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-Fa = int(input("What temperature in fahrenheit do you want me to convert?"))
+Fa = int(input("What temperature in Fahrenheit do you want me to convert?"))
 def F2C(Fa):
 	return (Fa-32)*5/9
 print (F2C(Fa))
+
+Ce = int(input("What temperature in Celsius do you want me to convert?"))
+def C2F(Ce):
+	return (Ce*(9/5))+32
+print (C2F(Ce))
